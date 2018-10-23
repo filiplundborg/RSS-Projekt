07a50b8b9d49880ce18d5c74a8af3ed14ba71efd
filiplundborg <43001096+filiplundborg@users.noreply.github.com>
2018-10-23 14:01:39 +0200

    Avsnittsklass

---
+++ Main_Form/Main_Form/Avsnitt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Form
+{
+    public class Avsnitt
+    {
+        public string Namn { get; set; }
+        public string Beskrivning { get; set; }
+
+
+    }
+}
