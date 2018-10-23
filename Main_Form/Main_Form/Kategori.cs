22d24249b41c1bf23bc23267914195ac830a2556
Samuel lindquist <Samme_li22@hotmail.com>
2018-10-23 13:14:09 +0200

    Lagt till klass Kategori

---
+++ Main_Form/Main_Form/Kategori.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Form
+{
+    public class Kategori
+    {
+        public string Category{ get; set; }
+
+    }
+}
