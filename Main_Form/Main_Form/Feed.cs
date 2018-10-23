bf95dcbe938d86eb7b5c707cb22100d460e18085
Oskar Ågren <oagren@outlook.com>
2018-10-23 14:01:54 +0200

    Feed med konstruktor

--- Main_Form/Main_Form/Feed.cs
+++ Main_Form/Main_Form/Feed.cs
@@ -13,5 +13,10 @@ namespace Main_Form
         public string UppdateringsInterval { get; set; }
         public Kategori Kategorin { get; set; }
         public Frekvens Frekvensen { get; set; }
+        public RssList<Episodes> Listan { get; set; }
+
+        public Feed() {
+            this.Listan = new RssList<Episodes>;
+        }
     }
 }
