bd2ec8e23dc9d5b460d9aeb10d29bc6117f0d7fe
Samuel lindquist <Samme_li22@hotmail.com>
2018-10-24 14:50:20 +0200

    Listwork

--- Main_Form/Main_Form/Form1.cs
+++ Main_Form/Main_Form/Form1.cs
@@ -13,10 +13,12 @@ namespace Main_Form
     public partial class Form1 : Form
     {
         private FeedList feedlist;
+        private RssList<Avsnitt> EpisodeList;
         public Form1()
         {
             InitializeComponent();
             feedlist = new FeedList();
+            EpisodeList = new RssList<Avsnitt>();
             cboxNyUppdatFrekvens.Items.Add("Var 5:e minut");
             cboxNyUppdatFrekvens.Items.Add("Var 10:e minut");
             cboxNyUppdatFrekvens.Items.Add("Var 15:e minut");
@@ -25,8 +27,19 @@ namespace Main_Form
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //feedlist = feedlist.Load();
+            feedlist = feedlist.Load();
+            Updatelist();
         }
+
+        public void Updatelist() {
+            lvPodcasts.Items.Clear();
+            foreach (var item in feedlist)
+            {
+                var itemsToAdd = new ListViewItem(new[] { item.AntalAvsnitt().ToString(), item.Namn  });
+                lvPodcasts.Items.Add(itemsToAdd);
+            }
+        }
+
 
         private void btnNyPod_Click(object sender, EventArgs e)
         {
@@ -36,11 +49,42 @@ namespace Main_Form
                 Feed feed = new Feed(tbNyUrl.Text);
                 feedlist.Add(feed);
                 feedlist.Save();
+                Updatelist();
             }
             catch (ArgumentException ){
                 MessageBox.Show("Samtliga alternativ måste vara ifyllda");
             }
             
+        }
+
+        private void lvPodcasts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lvPodcasts.SelectedItems.Count > 0)
+            {
+                int items = lvPodcasts.Items.IndexOf(lvPodcasts.SelectedItems[0]);
+                FillEpisodeList(items);
+            }
+        }
+
+        private void FillEpisodeList(int items) {
+            lboxAvsnitt.Items.Clear();
+            Feed feed = feedlist[items];
+            EpisodeList = feed.getListan();
+            foreach (var item in EpisodeList)
+            {
+                lboxAvsnitt.Items.Add(item.Namn);
+            }
+        }
+
+        private void lboxAvsnitt_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = lboxAvsnitt.SelectedIndex;
+            GetDescription(index);
+        }
+
+        public void GetDescription(int index) {
+            Avsnitt avsnitt = EpisodeList[index];
+            rtbBeskrivningAvsnitt.Text = avsnitt.Beskrivning.Replace("<p>", "").Replace("</p>", "");
         }
     }
 }
